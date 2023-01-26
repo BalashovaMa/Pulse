eb929265adfe2a3a706543f844505c24184d8417
BalashovaMa <balashova.m.nd@gmail.com>
2023-01-26 16:22:06 +0200

    feat: add scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -17,15 +17,15 @@ $(document).ready(function () {
         ]
     });
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -45,13 +45,13 @@ $(document).ready(function () {
         $('.overlay').fadeOut();
         $('.modal').fadeOut('slow');
     });
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
     })
-  
+
     function validateForms(form) {
         $(form).validate({
             rules: {
@@ -76,5 +76,20 @@ $(document).ready(function () {
     validateForms('#consultation-form');
 
     $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+    //Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    })
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
 
